985e107f60cd3398b6bf29a32ba94a9cdd483f08
Rutuja Kenjalkar <130883652+rutujakenjalkar@users.noreply.github.com>
2025-09-09 21:56:44 +0530

    Add files via upload

---
+++ OOP/cognizant prcatice.py
@@ -0,0 +1,89 @@
+# PYTHON HAS THE FOLLOWING FEATURES
+"""
+1. Simple to learn 
+2. Open-Source 
+3.Versatile :IE USED for various application like ml data ,sci ,web dev 
+4.dynamically typed - type of varaible its value are all checked at runtime 
+5.Object -Oriented Programming language .
+6. emdbedded - python code can run along with any programming language,
+7.IT IS CASE SENSITIVE PROGRAMMING LANGUAGE
+"""
+
+# Python files are saved with .py extensions
+
+#Comments in python -lines which are not executed
+#- denotes single line commment
+""" multiline commmnets"""
+
+#PYHTON CONSOLE CAN BE ACCESSED IN TWO WAYS 
+"""
+1.DIRECTTLLY RUNNING THE INTERPRETER BY COMMMAND LINE BY THE COMMAND
+    -- python <name of file>.py
+
+2. Using the GUI of of pyhton IDLE -Integrated learning and developement environment
+    -- wroks on different platform like Windows , Linux ,Mac
+"""
+
+# VARIABLE MEMERORY PARTS OF THE COMPUTER WHERE VALUES CAN BE STORED AND THESE VALUES CAN VARY .
+#the values can be of dfferent types known as data types . STANDARD DATA TYPES - numbers,string ,list,tuple,dictionary
+#VARIABLES ARE KNOWN BY THEIR NAMES MAKING VARIBALES IDENTIFIERS.
+
+# rules for deciding the names -
+""" 
+1. first chracter of name can be _ of character values
+2. name can contain digits(0-9), characters a-z A-z and _
+3.Nospecial characters allowed.
+"""
+
+print("RUTUJA KENJALKAR")
+
+#ASSIGNMENT OF VALUES = (assignment operator) 
+a=10
+print(a)
+a="rutuja"
+print(a)
+
+#ONE VALUE TO MULITPLE VARIABLES 
+a=b=c=1
+print("a:",a,"b:",b,"c:",c)
+
+#DIFFERENT VALUES TO DIFFERNT VARIABLES 
+x,y=1,2
+print("x:",x,"y:",y)
+
+#MUiltple statement in a single line (use semicolon to separate the lines)
+print("python");print("programme");
+#or 
+print("python");print("programme")
+
+#BOOLEAN (TRUE OR FALSE VALUES OF AVRAIBLE)
+X=12
+print(X>10)
+
+print(100==12)
+
+#input function - to take input from users 
+""" the input function is always take input of datatype string """
+
+"""x=input("what is your name?")"""
+
+# to take input as intrger
+#y=int(input("what is ur age?"))
+#print(type(y))
+
+
+#type function is used to find the data type of the varaible
+"""     SYNTAX : type(<name>)   """
+
+#print(type(x))
+
+
+#INLATEST PYTHON 3.X VERSIONS THE raw_input() funtion 
+""" ARITHMATIC OPERATORS"""
+print(1+2)
+print(12-4)
+print(2*4)
+print(3**2)
+print(17/4)
+print(17//4)
+
