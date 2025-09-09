e94201beb135277c08ab02cd6f95c76b461a15d5
Rutuja Kenjalkar <130883652+rutujakenjalkar@users.noreply.github.com>
2025-09-09 22:17:41 +0530

    Add files via upload

---
+++ OOP/Strings.py
@@ -0,0 +1,37 @@
+#CONCATENATION 
+
+print("hello"+"world")
+#to add number to string store the number in the string
+print("python"+"3.14")
+
+str1="1"
+str2="a"
+print(str1+str2)
+
+
+#STRING MULITPLICATION 
+#YOU CAN ONLY MULITPLY STRING WITH AN INTERGER N ,U CANNOT MULITPLY WITH ANOTHER STRING OR FLOAT
+
+print("hell"*4)
+
+#slicing of strings 
+col="0123456789"
+
+print(col[0])
+print(col[1:5])
+print(col[-1])
+print(col[::2])
+#revreseing a string
+print(col[::-1])
+
+print(col[:6])
+print(col[6:])
+
+"""TUPLES values separated by commas enclosed between parenthese , values can take differnet data types . also they
+are immutable"""
+
+tup1=(1,23,4,"abcd")
+print(tup1)
+
+tup2=(1,2,34)
+print(tup2)
